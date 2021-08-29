5103e54aa52c93090ef6c5862030f567367b0f13
Nilesh Kumar Gupta <nileshkumar1114@gmail.com>
2021-08-29 18:04:10 +0530

    add collision bw ball and paddle

--- js/index.js
+++ js/index.js
@@ -6,6 +6,8 @@ var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 
 
 var ball; 
+var paddle;
+
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -14,22 +16,34 @@ function preload() {
     game.stage.backgroundColor = "#eee";
 
     game.load.image('ball', 'img/ball.png');
+    game.load.image('paddle', 'img/paddle.png');
 };
 
 function create() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
+
     ball = game.add.sprite(50, 100, 'ball');
+    ball.anchor.set(0.5);
+
+    paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
+    paddle.anchor.set(0.5, 1);
 
     game.physics.enable(ball, Phaser.Physics.ARCADE);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
 
     ball.body.velocity.set(150, -150);
-    ball.body.gravity.y = 100;
-
+    paddle.body.immovable = true;
 
 };
 
 
 function update() {
+
+    game.physics.arcade.collide(ball, paddle);
+    paddle.x = game.input.x || game.world.width * 0.5;
 
 };
