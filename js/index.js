887e6561feeac2883a88f0c3f50acede4707fbef
Nilesh Kumar Gupta <nileshkumar1114@gmail.com>
2021-08-29 12:39:09 +0530

    initialize and Add basic js script

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World!")
+
+var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+    preload: preload,
+    create: create, 
+    update: update
+});
+
+function preload() {};
+function create() {};
+function update() {};
